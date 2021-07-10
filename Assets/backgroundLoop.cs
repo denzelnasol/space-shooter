699137ab8b2a504fbbc8962bb894fbfd2c3a70ee
dnasol <denzelnasol@gmail.com>
2021-07-09 19:56:46 -0700

    Add scrolling effect for background

---
+++ Assets/backgroundLoop.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundLoop : MonoBehaviour
+{
+    public GameObject[] levels;
+    private Camera mainCamera;
+    private Vector2 screenBounds;
+
+    private void Start()
+    {
+        mainCamera = gameObject.GetComponent<Camera>();
+        screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
+        foreach(GameObject obj in levels)
+        {
+            loadChildObjects(obj);
+        }
+    }
+
+    void loadChildObjects(GameObject obj)
+    {
+        float objectHeight = obj.GetComponent<SpriteRenderer>().bounds.size.y;
+        int childsNeeded = (int)Mathf.Ceil(screenBounds.x * 2 / objectHeight);
+        GameObject clone = Instantiate(obj) as GameObject;
+        for (int i = 0; i <= childsNeeded; i++)
+        {
+            GameObject c = Instantiate(clone) as GameObject;
+            c.transform.SetParent(obj.transform);
+            c.transform.position = new Vector3(obj.transform.position.x, objectHeight * i, obj.transform.position.z);
+            c.name = obj.name + i;
+        }
+        Destroy(clone);
+        Destroy(obj.GetComponent<SpriteRenderer>());
+    }
+    void repositionChildObjects(GameObject obj)
+    {
+        Transform[] children = obj.GetComponentsInChildren<Transform>();
+        if (children.Length > 1)
+        {
+            GameObject firstChild = children[1].gameObject;
+            GameObject lastChild = children[children.Length - 1].gameObject;
+            float halfObjectHeight = lastChild.GetComponent<SpriteRenderer>().bounds.extents.y;
+            if (transform.position.y + screenBounds.y > lastChild.transform.position.y + halfObjectHeight)
+            {
+                firstChild.transform.SetAsLastSibling();
+                firstChild.transform.position = new Vector3(lastChild.transform.position.x, lastChild.transform.position.y + halfObjectHeight * 2, lastChild.transform.position.z);
+            } else if (transform.position.y - screenBounds.y < firstChild.transform.position.y - halfObjectHeight)
+            {
+                lastChild.transform.SetAsFirstSibling();
+                lastChild.transform.position = new Vector3(firstChild.transform.position.x, lastChild.transform.position.y - halfObjectHeight * 2, firstChild.transform.position.z);
+            }
+        }
+    }
+    private void LateUpdate()
+    {
+        foreach(GameObject obj in levels)
+        {
+            repositionChildObjects(obj);
+        }
+    }
+}
