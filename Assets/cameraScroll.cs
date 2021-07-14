3bf73ba7fd50e18b71a0c368df34d256624ca458
dnasol <denzelnasol@gmail.com>
2021-07-13 22:16:02 -0700

    Add camera scroll

---
+++ Assets/cameraScroll.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScroll : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        transform.Translate(0, speed, 0);
+    }
+}
